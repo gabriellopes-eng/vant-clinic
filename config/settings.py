b222fcaa33b2fc25ee12c0ca907f65e7547f6c5a
Gabriel Lopes <gabriel.lopes.albuquerque@gmail.com>
2026-05-24 20:23:29 -0300

    add core application modules to INSTALLED_APPS in settings

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core',
+    'clientes',
+    'profissionais',
+    'servicos',
+    'agendamentos',
+    'pagamentos',
+    'relatorios',
 ]
 
 MIDDLEWARE = [
